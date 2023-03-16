3dac06d94b8b75f0b6c91828a0b1bdec5c2ba167
newt239 <ysti2019.win2020.st@gmail.com>
2023-03-16 20:07:05 +0900

    add: select block

--- src/components/features/timeline/index.tsx
+++ src/components/features/timeline/index.tsx
@@ -1,18 +1,29 @@
 "use client";
 
-import { List } from "@mantine/core";
-import { useAtomValue } from "jotai";
+import { Button, List } from "@mantine/core";
+import { useAtom, useAtomValue } from "jotai";
 
-import { blocksAtom } from "@/store/jotai";
+import { blocksAtom, currentBlockIdAtom } from "@/store/jotai";
 
 const Timeline = () => {
   const blocks = useAtomValue(blocksAtom);
+  const [currentBlockId, setCurrentBlockId] = useAtom(currentBlockIdAtom);
+
+  const setBlockId = (block_id: string) => {
+    setCurrentBlockId(block_id);
+  };
 
   return (
     <List>
       {blocks.map((block) => (
         <List.Item key={block.id}>
           {block.id} - {block.content}
+          <Button
+            onClick={() => setBlockId(block.id)}
+            disabled={currentBlockId === block.id}
+          >
+            選択
+          </Button>
         </List.Item>
       ))}
     </List>
