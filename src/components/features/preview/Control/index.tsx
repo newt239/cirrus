8f189c96f6d2deb502b178e0d5a133b1f7110657
newt239 <ysti2019.win2020.st@gmail.com>
2023-03-16 21:14:56 +0900

    add: show current time at control component

--- src/components/features/preview/Control/index.tsx
+++ src/components/features/preview/Control/index.tsx
@@ -1,15 +1,23 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
+import { useInterval } from "@mantine/hooks";
 import { gsap } from "gsap";
 import { useAtomValue } from "jotai";
 
-import { Button } from "@/lib/mantine/core";
+import { Button, Flex } from "@/lib/mantine/core";
 import { blocksAtom } from "@/store/jotai";
 
 const Control: React.FC = () => {
   const blocks = useAtomValue(blocksAtom);
+  const [tl, setTl] = useState(gsap.timeline());
+  const [currentMs, setCurrentMs] = useState(0);
+  const interval = useInterval(() => {
+    if (tl.totalTime() === tl.time()) interval.toggle();
+    setCurrentMs(tl.time());
+  }, 10);
 
-  const tl = gsap.timeline();
   const playAnime = () => {
     for (const block of blocks) {
       const final_state = block.final_state as gsap.TweenVars;
@@ -20,6 +28,17 @@ const Control: React.FC = () => {
     }
     tl.play();
   };
-  return <Button onClick={playAnime}>再生</Button>;
+
+  useEffect(() => {
+    interval.start();
+    return interval.stop;
+  }, []);
+
+  return (
+    <Flex justify="space-between">
+      <Button onClick={playAnime}>再生</Button>
+      <div>{currentMs}ms</div>
+    </Flex>
+  );
 };
 export default Control;
