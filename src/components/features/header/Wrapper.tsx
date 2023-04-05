8a4903008faecd7a0560de85c6d2fe57c93ea130
newt239 <ysti2019.win2020.st@gmail.com>
2023-04-05 23:46:24 +0900

    chore: install modals provider

--- src/components/features/header/Wrapper.tsx
+++ src/components/features/header/Wrapper.tsx
@@ -12,6 +12,8 @@ import {
   ScrollArea,
 } from "~/lib/mantine/core";
 
+import { ModalsProvider } from "@mantine/modals";
+
 import SideBar from "../sidebar/SideBar";
 
 import DashboardHeader from "./DashboardHeader";
@@ -44,36 +46,38 @@ const Wrapper = ({ children }: Props) => {
       withGlobalStyles
       withNormalizeCSS
     >
-      <Header height="auto">
-        {pathname === "/" ? (
-          <HomeHeader />
-        ) : pathname === "/dashboard" ? (
-          <DashboardHeader />
-        ) : (
-          <StudioHeader />
-        )}
-      </Header>
-      <Flex>
-        {pathname !== "/" && pathname !== "/dashboard" ? (
-          <Navbar
-            fixed={false}
-            p="xs"
-            position={{ top: 0, left: 0 }}
-            width={{ base: 300 }}
-            zIndex={1}
+      <ModalsProvider>
+        <Header height="auto">
+          {pathname === "/" ? (
+            <HomeHeader />
+          ) : pathname === "/dashboard" ? (
+            <DashboardHeader />
+          ) : (
+            <StudioHeader />
+          )}
+        </Header>
+        <Flex>
+          {pathname !== "/" && pathname !== "/dashboard" ? (
+            <Navbar
+              fixed={false}
+              p="xs"
+              position={{ top: 0, left: 0 }}
+              width={{ base: 300 }}
+              zIndex={1}
+            >
+              <Navbar.Section component={ScrollArea} grow mx="-xs" px="xs">
+                <SideBar />
+              </Navbar.Section>
+            </Navbar>
+          ) : undefined}
+          <Box
+            sx={(theme) => ({ backgroundColor: theme.colors.gray[1] })}
+            w="100%"
           >
-            <Navbar.Section component={ScrollArea} grow mx="-xs" px="xs">
-              <SideBar />
-            </Navbar.Section>
-          </Navbar>
-        ) : undefined}
-        <Box
-          sx={(theme) => ({ backgroundColor: theme.colors.gray[1] })}
-          w="100%"
-        >
-          {children}
-        </Box>
-      </Flex>
+            {children}
+          </Box>
+        </Flex>
+      </ModalsProvider>
     </MantineProvider>
   );
 };
--- src/components/features/menubar/MenuBar.tsx
+++ src/components/features/menubar/MenuBar.tsx
@@ -1,14 +1,8 @@
-import { Box, Button, Flex, Menu } from "~/lib/mantine/core";
+"use client";
 
-import {
-  IconArrowsLeftRight,
-  IconFile,
-  IconMessageCircle,
-  IconPhoto,
-  IconSearch,
-  IconSettings,
-  IconTrash,
-} from "@tabler/icons-react";
+import { Box, Flex, Menu } from "~/lib/mantine/core";
+
+import { IconFile, IconSettings, IconTrash } from "@tabler/icons-react";
 
 import MenuBarButton from "~/components/blocks/MenuBarButton";
 
@@ -23,20 +17,9 @@ const MenuBar: React.FC = () => {
         </Menu.Target>
 
         <Menu.Dropdown>
-          <Menu.Label>Application</Menu.Label>
           <Menu.Item icon={<IconSettings size={14} />}>Settings</Menu.Item>
-          <Menu.Item icon={<IconMessageCircle size={14} />}>Messages</Menu.Item>
-          <Menu.Item icon={<IconPhoto size={14} />}>Gallery</Menu.Item>
-          <Menu.Item icon={<IconSearch size={14} />}>Search</Menu.Item>
-
-          <Menu.Divider />
-
-          <Menu.Label>Danger zone</Menu.Label>
-          <Menu.Item icon={<IconArrowsLeftRight size={14} />}>
-            Transfer my data
-          </Menu.Item>
           <Menu.Item color="red" icon={<IconTrash size={14} />}>
-            Delete my account
+            ファイルを削除する
           </Menu.Item>
         </Menu.Dropdown>
       </Menu>
