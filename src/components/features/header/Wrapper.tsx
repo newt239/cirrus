6ab6e6561d4e7b343b97ca5047b82f47029a14d5
newt239 <ysti2019.win2020.st@gmail.com>
2023-04-05 22:49:41 +0900

    change: remove AppShell

--- src/components/features/header/Wrapper.tsx
+++ src/components/features/header/Wrapper.tsx
@@ -4,6 +4,8 @@ import { usePathname } from "next/navigation";
 
 import {
   AppShell,
+  Box,
+  Flex,
   Header,
   MantineProvider,
   Navbar,
@@ -30,44 +32,35 @@ const Wrapper = ({ children }: Props) => {
           '"LINESeedJP", "Noto Sans JP", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
       }}
     >
-      <AppShell
-        header={
-          <Header display="flex" height={70} px="lg" py="sm">
-            {pathname === "/" ? (
-              <HomeHeader />
-            ) : pathname === "/dashboard" ? (
-              <DashboardHeader />
-            ) : (
-              <StudioHeader />
-            )}
-          </Header>
-        }
-        navbar={
-          pathname !== "/" && pathname !== "/dashboard" ? (
-            <Navbar
-              fixed={false}
-              p="xs"
-              position={{ top: 0, left: 0 }}
-              width={{ base: 300 }}
-            >
-              <Navbar.Section component={ScrollArea} grow mx="-xs" px="xs">
-                <Menus />
-              </Navbar.Section>
-            </Navbar>
-          ) : undefined
-        }
-        padding="md"
-        styles={(theme) => ({
-          main: {
-            backgroundColor:
-              theme.colorScheme === "dark"
-                ? theme.colors.dark[8]
-                : theme.colors.gray[0],
-          },
-        })}
-      >
-        {children}
-      </AppShell>
+      <Header display="flex" height={70} px="lg" py="sm">
+        {pathname === "/" ? (
+          <HomeHeader />
+        ) : pathname === "/dashboard" ? (
+          <DashboardHeader />
+        ) : (
+          <StudioHeader />
+        )}
+      </Header>
+      <Flex>
+        {pathname !== "/" && pathname !== "/dashboard" ? (
+          <Navbar
+            fixed={false}
+            p="xs"
+            position={{ top: 0, left: 0 }}
+            width={{ base: 300 }}
+          >
+            <Navbar.Section component={ScrollArea} grow mx="-xs" px="xs">
+              <Menus />
+            </Navbar.Section>
+          </Navbar>
+        ) : undefined}
+        <Box
+          sx={(theme) => ({ backgroundColor: theme.colors.gray[1] })}
+          w="100%"
+        >
+          {children}
+        </Box>
+      </Flex>
     </MantineProvider>
   );
 };
