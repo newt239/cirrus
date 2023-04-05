fcec09da3d33b86500c4d4e664bf411b0d7ae958
newt239 <ysti2019.win2020.st@gmail.com>
2023-04-05 17:25:18 +0900

    add: hero image

--- src/components/features/home/Hero.tsx
+++ src/components/features/home/Hero.tsx
@@ -1,16 +1,15 @@
-import { Box, Text, Title } from "@mantine/core";
+import { BackgroundImage, Box, Text, Title } from "~/lib/mantine/core";
 
 const Hero: React.FC = () => {
   return (
-    <Box
-      sx={{
-        textAlign: "center",
-        margin: "30vh 0px",
-      }}
-    >
-      <Title order={1}>動画制作を、もっと手軽に。</Title>
-      <Text>右上のサインインボタンをクリック</Text>
-    </Box>
+    <BackgroundImage h="calc(100vh - 105px)" src="/images/hero.png">
+      <Box p="lg" sx={{ textShadow: "2px 2px 2px rgba(0,0,0,0.5)" }}>
+        <Title color="white" order={1}>
+          動画制作を、もっと手軽に。
+        </Title>
+        <Text color="white">右上のサインインボタンをクリック</Text>
+      </Box>
+    </BackgroundImage>
   );
 };
 
