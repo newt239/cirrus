d2fbb6776a0ad3d2845f43df20ff5974158665eb
newt239 <ysti2019.win2020.st@gmail.com>
2023-05-07 11:46:39 +0900

    add: block list like timeline

--- src/components/features/timeline/Timeline.tsx
+++ src/components/features/timeline/Timeline.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Box, Button, List, Title } from "~/libs/mantine/core";
+import { Box, Paper, Title } from "~/libs/mantine/core";
 
 import { useAtom, useAtomValue } from "jotai";
 
@@ -17,19 +17,25 @@ const Timeline: React.FC = () => {
   return (
     <Box p="sm">
       <Title order={3}>ブロック一覧</Title>
-      <List>
+      <Box bg="gray.3" h="200px" pos="relative" w="100%">
         {blocks.map((block) => (
-          <List.Item key={block.id}>
-            {block.id}
-            <Button
-              disabled={currentBlockId === block.id}
-              onClick={() => setBlockId(block.id)}
-            >
-              選択
-            </Button>
-          </List.Item>
+          <Paper
+            className="overflow-hidden whitespace-nowrap text-xs"
+            component="button"
+            h={30}
+            key={block.id}
+            left={block.start / 50}
+            onClick={() => setBlockId(block.id)}
+            pos="absolute"
+            shadow="sm"
+            top={block.layer * 30}
+            w={block.duration / 50}
+            withBorder
+          >
+            {block.name}
+          </Paper>
         ))}
-      </List>
+      </Box>
     </Box>
   );
 };
