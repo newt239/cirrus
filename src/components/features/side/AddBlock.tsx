f3faf9bcbc320e2ec4a220c03b4fcc40655d30e5
newt239 <ysti2019.win2020.st@gmail.com>
2023-06-17 15:17:42 +0900

    remove: unused jotai atoms

--- src/components/features/side/AddBlock.tsx
+++ src/components/features/side/AddBlock.tsx
@@ -3,9 +3,7 @@
 import { Box, Button } from "~/libs/mantine/core";
 
 import { IconPlus } from "@tabler/icons-react";
-import { useSetAtom } from "jotai";
 
-import { setBlockAtom } from "~/store/jotai";
 import { addBlock } from "~/utils/db";
 
 type Props = {
@@ -13,16 +11,8 @@ type Props = {
 };
 
 const AddBlock: React.FC<Props> = ({ project_id }) => {
-  const setNewBlock = useSetAtom(setBlockAtom);
-
   const addNewBlock = async () => {
     const newBlock = await addBlock(project_id);
-    if (newBlock) {
-      setNewBlock(["add", newBlock]);
-      console.log(newBlock);
-    } else {
-      console.log("error");
-    }
   };
 
   return (
